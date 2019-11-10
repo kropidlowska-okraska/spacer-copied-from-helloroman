90ee06ee9497f1963015cc09a73b0c317a5dc252
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2019-11-10 23:27:26 +0100

    Add hero image component

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="heroImage" />
+</template>
+
+<script>
+export default {
+  name: 'HeroImage',
+};
+</script>
+
+<style scoped>
+  .heroImage {
+    position: fixed;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    z-index: -1;
+    background-image: url("../assets/header_image.jpeg");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
