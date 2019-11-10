78b13458f2ec601602a58e30911cc0b778b6c1ed
kropidlowska-okraska <m.k.kropidlowska@gmail.com>
2019-11-10 23:26:34 +0100

    Create SearchInput component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,66 @@
+<template>
+    <input
+      type="text"
+      name="search"
+      id="search"
+      :class="{ dark }"
+      :value="value"
+      @input="handleChange"
+    />
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  input {
+    margin-top: 90px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 250px;
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid white;
+    background: none;
+    color: white;
+    font-size: 22px;
+    font-family: 'Varela Round', sans-serif;
+    text-align: center;
+    outline: none;
+    transition: box-shadow .5s;
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(255, 255, 255, 0.3);
+    }
+  }
+
+  .dark {
+    color: darkblue;
+    border-bottom-color: darkblue;
+
+    &:focus {
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(darkblue, 0.3);
+    }
+  }
+</style>
